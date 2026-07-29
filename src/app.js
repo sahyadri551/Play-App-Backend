b8537f2f022540b78066eb7ac9e8cec828849429
sahyadri551 <sahyadri2413082@akgec.ac.in>
2026-07-30 00:28:53 +0530

    changed app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+app.disable("x-powered-by");
+
+
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+import { app } from "./app.js";
 
-connectDB();
+try {
+    await connectDB();
+    app.listen(process.env.PORT || 8000,
+        () => console.log(`Server running on port ${process.env.PORT}`));
+} catch (error) {
+    console.log("Error connecting to MongoDB", error);
+    process.exit(1);
+}
