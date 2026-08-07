58288b34dc1a36514870a7c9c34a0272728df842
sahyadri551 <sahyadri2413082@akgec.ac.in>
2026-08-07 23:55:44 +0530

    feat: integrate Cloudinary for file uploads, add multer middleware, and update environment configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 } from 'cloudinary';
+import fs from 'node:fs';
+import chalk from 'chalk';
+
+v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async (filePath) => {
+    try {
+        if (!fs.existsSync(filePath)) {
+            throw new Error(`File not found: ${filePath}`);
+        }
+        const result = await v2.uploader.upload(filePath, {
+            resource_type: 'auto',
+        });
+        fs.unlinkSync(filePath);
+        console.log(chalk.green(`->>> File uploaded to Cloudinary: ${result.secure_url}`));
+        return result;
+    } catch (error) {
+        console.error(chalk.red('Error uploading to Cloudinary:', error));
+        fs.unlinkSync(filePath);
+        return null;
+    }
+};
+
+export default uploadOnCloudinary;
